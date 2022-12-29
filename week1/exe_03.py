9a6ab8406f058837d215f0ede2a1c310b9b83ff7
syed asif <sydasif78@gmail.com>
2022-12-29 13:16:13 +0500

    format code with black

--- week1/exe_03.py
+++ week1/exe_03.py
@@ -1,5 +1,8 @@
 """
-3.   Create three different variables the first variable should use all lower case characters with underscore ( _ ) as the word separator. The second variable should use all upper case characters with underscore as the word separator. The third variable should use numbers, letters, and underscore, but still be a valid variable Python variable name.
+3. Create three different variables the first variable should use all lower case characters with
+underscore ( _ ) as the word separator. The second variable should use all upper case characters
+with underscore as the word separator. The third variable should use numbers, letters, and
+underscore, but still be a valid variable Python variable name.
 
 Make all three variables be strings that refer to IPv6 addresses.
 
@@ -8,6 +11,7 @@ Use the from future technique so that any string literals in Python2 are unicode
 compare if variable1 equals variable2
 compare if variable1 is not equal to variable3
 """
+
 from __future__ import unicode_literals
 
 ip_addr = "3001:0da8:75a3:0000:0000:8a2e:0370:873"
